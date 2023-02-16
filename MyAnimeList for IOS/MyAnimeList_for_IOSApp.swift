9ebe9b73ebed9f7da2cd842a0d8dfe7fb9592e61
Haydar <haydar919@gmail.com>
2023-02-16 15:32:39 +0100

    Initial Commit

---
+++ MyAnimeList for IOS/MyAnimeList_for_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimeList_for_IOSApp.swift
+//  MyAnimeList for IOS
+//
+//  Created by Haydar Bahr on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimeList_for_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
