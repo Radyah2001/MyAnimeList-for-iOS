dd1860b43ffe969ad80e47a884a20cc0e3c46f32
Haydar <haydar919@gmail.com>
2023-03-09 15:46:25 +0100

    Made a search icon on the home page

--- MyAnimeList for IOS/ContentView.swift
+++ MyAnimeList for IOS/ContentView.swift
@@ -8,48 +8,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    // do nothing, since this is already the current view
-                }) {
+        NavigationView{
+            VStack {
+                HStack {
+                    Button(action: {
+                        // do nothing, since this is already the current view
+                    }) {
+                        Image("MAL")
+                            .resizable()
+                            .font(.title)
+                            .scaledToFit()
+                            .foregroundColor(.blue)
+                            .frame(width:40, height: 40)
+                            .padding()
+                    }
+                    Text("MyAnimeList")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 5)
+                        .padding(.leading, 20)
+                    Spacer()
+                    NavigationLink(destination: SearchView()) {
+                        Image(systemName: "magnifyingglass")
+                            .font(.largeTitle)
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                }
+                ScrollView{
                     Image("MAL")
                         .resizable()
-                        .font(.title)
                         .scaledToFit()
-                        .foregroundColor(.blue)
-                        .frame(width:40, height: 40)
-                        .padding()
-                }
-                Text("MyAnimeList")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.bottom, 5)
-                    .padding(.leading, 20)
-                Spacer()
-            }
-            ScrollView{
-                Image("MAL")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-            }
-            Text("This is the frontpage view of my app.")
-                .font(.title2)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-            NavigationView{
-                VStack{
-                    NavigationLink(destination: SearchView()){Text("Search")}
+                        .frame(width: 100, height: 100)
                 }
             }
+            .navigationTitle("Home")
+            
             
             Spacer()
         }
         .frame(maxWidth:.infinity,maxHeight: .infinity)
         .background(Color("Color1"))
-        .tint(Color.white)
+        .tint(Color.black)
     }
 }
 
--- MyAnimeList for IOS/SearchView.swift
+++ MyAnimeList for IOS/SearchView.swift
@@ -39,7 +39,7 @@ struct SearchView: View {
                         }
                     }
                     .navigationTitle("Anime Search")
-                    
+                
                 }
             }
         }
